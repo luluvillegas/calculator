da5de1de2abf447c90a1e8780bfb040f0cec7809
luluvillegas <lucrecia.villegas.gomez@gmail.com>
2024-03-07 11:46:24 -0500

    Created function that populate the display and clean it.
    Created functions to populate the display when pressing numbers and the function that cleans the whole display.

--- calculator.js
+++ calculator.js
@@ -31,6 +31,28 @@ function operate(fNumber, sNumber, op) {
   }
 }
 
+function populateDisplay(str) {
+  let number = (display.textContent += str);
+  //console.log(number);
+}
+
 let firstNumber;
 let secondNumber;
 let operator;
+//let first = false;
+//let second = false;
+const display = document.querySelector(`.display`);
+
+const numbersPad = Array.from(document.querySelectorAll(`.numbers > button`));
+//console.log(numbers);
+for (let i = 0; i <= numbersPad.length - 1; i++) {
+  numbersPad[i].addEventListener(`click`, (e) => {
+    //console.log(e.target.textContent);
+    populateDisplay(e.target.textContent);
+  });
+}
+
+const cleanBtn = document.querySelector(`.clean`);
+cleanBtn.addEventListener(`click`, () => {
+  display.innerHTML = ``;
+});
